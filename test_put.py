cc6d716a90df76e6b7f4acf363a78a5a13eefb9a
5ia <new5ia@yahoo.com>
2015-11-15 19:59:14 +0100

    Update test_put.py

--- test_put.py
+++ test_put.py
@@ -4,114 +4,129 @@ import unittest
 
 class TestUpdate(unittest.TestCase):
     
-    valid_usr='myself'
-    valid_pass='mypass'
-    app_name='MyApp'
-    domain="https://my.domain.com"
-    appId = '123'
-    permissions = [ "A", "C" ] #WHAT ARE THE VALID PERMISSIONS? ARE THERE INVALID ONES AS WELL?
-    inv_usr='john' #user that is not registered
-    inv_pass='random'
-    usr2='ben'
-    psw2='benspass'
-    app_new_name='MyNewApp'
-    new_domain="https://my.newdomain.com"
-    inv_appId="8979" # there should be no app with this ID
+    VALID_USR='myself'
+    VALID_PASS='mypass'
+    APP_NAME='MyApp'
+    DOMAIN="https://my.domain.com"
+    APPID = '123'
+    PERMISSIONS = [ "A", "C" ] #WHAT ARE THE VALID PERMISSIONS? ARE THERE INVALID ONES AS WELL?
+    INV_USR='john' #user that is not registered
+    INV_PASS='random'
+    USR2='ben'
+    PSW2='benspass'
+    APP_NEW_NAME='MyNewApp'
+    NEW_DOMAIN="https://my.newdomain.com"
+    INV_APPID="8979" # there should be no app with this ID
     
     def test_valid_auth_data(self):
         """ Test with correct authentication and valid data"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code, 200)
+        
     def test_missing_auth(self):
         """ Test request without any authentication"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
         r=requests.put(url,data=mydata)
         self.assertEqual(r.status_code,403)
+        
     def test_missing_usr(self):
         """ Test request without user value passed in auth"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=('', self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=('', self.VALID_PASS))
         self.assertEqual(r.status_code,403)
+        
     def test_missing_pass(self):
         """ Test request without password value passed in auth"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, ''))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, ''))
         self.assertEqual(r.status_code,403)
+        
     def test_wrong_usr(self):
         """ Test request with unexisting user value passed in auth"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.inv_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.INV_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,403)
+        
     def test_wrong_pass(self):
         """ Test request with incorrect password value passed in auth"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.inv_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.INV_PASS))
         self.assertEqual(r.status_code,403)
+        
     def test_wrong_owner(self):
         """ Test request with user that is not the owner of the app"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.usr2, self.psw2))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.USR2, self.PSW2))
         self.assertEqual(r.status_code,403)
+        
     def test_name_update(self):
         """ Test request trying to update application name"""
-        mydata = {"name" : self.app_new_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NEW_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,403)
+        
     def test_domain_update(self):
         """ Test request trying to update domain name"""
-        mydata = {"name" : self.app_name, "domain" : self.new_domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.NEW_DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,403)
+        
     def test_missing_body(self):
         """ Test request without a body"""
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url, auth=(self.valid_usr, self.valid_pass))
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,400)
+        
     def test_missing_appname(self):
         """ Test request without app name"""
-        mydata = { "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = { "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.APP_NAME))
         self.assertEqual(r.status_code,400)
+        
     def test_missing_domain(self):
         """ Test request without a domain"""
-        mydata = {"name" : self.app_name, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,400)
+        
     def test_missing_perms(self):
         """ Test request without permissions part"""
-        mydata = {"name" : self.app_name, "domain" : self.domain}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,400)
+        
     def test_unknown_element(self):
         """ Test request with additional part in body that is not defined in specs"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions, "new_option": "myoption"}
-        url = self.domain + '/apps/' + self.appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS, "new_option": "myoption"}
+        url = self.DOMAIN + '/apps/' + self.APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,400)
+        
     def test_missing_appid(self):
         """ Test request without application Id"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' 
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' 
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,500)
+        
     def test_missing_app(self):
         """ Test request for non-existant application"""
-        mydata = {"name" : self.app_name, "domain" : self.domain, "permissions" : self.permissions}
-        url = self.domain + '/apps/' + self.inv_appId
-        r=requests.put(url,data=mydata, auth=(self.valid_usr, self.valid_pass))
+        mydata = {"name" : self.APP_NAME, "domain" : self.DOMAIN, "permissions" : self.PERMISSIONS}
+        url = self.DOMAIN + '/apps/' + self.INV_APPID
+        r=requests.put(url,data=mydata, auth=(self.VALID_USR, self.VALID_PASS))
         self.assertEqual(r.status_code,404)
         
 
